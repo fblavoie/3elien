987f10b3f5c18f31d1048285722800ffa1377685
fblavoie <39136488+fblavoie@users.noreply.github.com>
2025-12-24 11:00:33 -0500

    add mary

--- aventures2/level2.js
+++ aventures2/level2.js
@@ -1,7 +1,5 @@
 sprites_l = [ 
-			
 				[6,6,"eric2.png",[ ["VOUS (FRANCE-ÉLAINE DURANCEAU)","C'est mon collègue Éric Caire. Vaut mieux ne pas le déranger. Dès qu'il touche à de quoi, ça se passe mal."],
-
 				],[]],
 				[2,1,"hide.png",[],[]],
 				[2,2,"hide.png",[],[]],
@@ -13,7 +11,16 @@ sprites_l = [
 				[4,2,"hide.png",[],[]],
 				[4,3,"hide.png",[],[]],
 				[5,2,"hide.png",[],[]],
-				
+				[2,6,"empty.png",[ 
+									["ÉCRITEAU","Her Majesty the Queen of Quebec, Mary Simon."],
+									["ÉCRITEAU","French description not available."],
+								]
+				],
+				[2,7,"empty.png",[ 
+									["ÉCRITEAU","Her Majesty the Queen of Quebec, Mary Simon."],
+									["ÉCRITEAU","French description not available."],
+								]
+				],
 			];
 
 
@@ -62,13 +69,15 @@ tiles = [
 				["block",0,0,"up"],["block",0,1,"up"],["block",0,2,"up"],["block",0,3,"up"],["block",0,4,"up"],["block",0,5,"up"],["block",0,6,"up"],["block",0,7,"up"],["block",0,8,"up"],["block",0,9,"up"],
 				["block",1,0,"up"],["block",1,1,"up"],["block",1,2,"up"],["block",1,3,"up"],["block",1,4,"up"],["block",1,5,"up"],["block",1,6,"up"],["block",1,7,"up"],["block",1,8,"up"],["block",1,9,"up"],
 				["block",2,0,"up"],["road",2,1,"up"],["road",2,2,"up"],["road",2,3,"up"],["block",2,4,"up"],["block",2,5,"up"],["block",2,6,"up"],["block",2,7,"up"],["block",2,8,"up"],["block",2,9,"up"],
-				["block",3,0,"up"],["road",3,1,"up"],["road",3,2,"up"],["road",3,3,"up"],["block",3,4,"up"],["block",3,5,"up"],["block",3,6,"up"],["block",3,7,"up"],["block",3,8,"up"],["block",3,9,"up"],
-				["block",4,0,"up"],["road",4,1,"up"],["road",4,2,"up"],["road",4,3,"up"],["block",4,4,"up"],["block",4,5,"up"],["block",4,6,"up"],["block",4,7,"up"],["block",4,8,"up"],["block",4,9,"up"],
-				["block",5,0,"up"],["block",5,1,"up"],["road",5,2,"up"],["block",5,3,"up"],["block",5,4,"up"],["block",5,5,"up"],["block",5,6,"up"],["block",5,7,"up"],["block",5,8,"up"],["block",5,9,"up"],
+				["block",3,0,"up"],["road",3,1,"up"],["road",3,2,"up"],["road",3,3,"up"],["block",3,4,"up"],["road",3,5,"up"],["road",3,6,"up"],["road",3,7,"up"],["road",3,8,"up"],["block",3,9,"up"],
+				["block",4,0,"up"],["road",4,1,"up"],["road",4,2,"up"],["road",4,3,"up"],["block",4,4,"up"],["road",4,5,"up"],["road",4,6,"up"],["road",4,7,"up"],["road",4,8,"up"],["block",4,9,"up"],
+				["block",5,0,"up"],["block",5,1,"up"],["road",5,2,"up"],["block",5,3,"up"],["block",5,4,"up"],["road",5,5,"up"],["block",5,6,"up"],["block",5,7,"up"],["block",5,8,"up"],["block",5,9,"up"],
 				["block",6,0,"up"],["block",6,1,"up"],["road",6,2,"up"],["block",6,3,"up"],["road",6,4,"up"],["road",6,5,"up"],["road",6,6,"up"],["road",6,7,"up"],["road",6,8,"up"],["road",6,9,"up"],
 				["block",7,0,"up"],["road",7,1,"up"],["road",7,2,"up"],["road",7,3,"up"],["road",7,4,"up"],["road",7,5,"up"],["road",7,6,"up"],["block",7,7,"up"],["block",7,8,"up"],["block",7,9,"up"],
 				["block",8,0,"up"],["road",8,1,"up"],["road",8,2,"up"],["road",8,3,"up"],["road",8,4,"up"],["road",8,5,"up"],["road",8,6,"up"],["block",8,7,"up"],["block",8,8,"up"],["block",8,9,"up"],
 				["block",9,0,"up"],["block",9,1,"up"],["block",9,2,"up"],["block",9,3,"up"],["block",9,4,"up"],["block",9,5,"up"],["block",9,6,"up"],["block",9,7,"up"],["block",9,8,"up"],["block",9,9,"up"],
+				["big",1,6],
+				["mary",1,6]
 
 
 		
